58bfa1f8d68050350a3da4d4133bb6c9bc5fba6e
Tushar <tsupe222@gmail.com>
2021-12-14 17:12:27 +0530

    added Operators in course-2

---
+++ Implement OOPS using JAVA with Data Structures and Beyond/Assignment-1/Operators/PostIncrementAndDecrement.java
@@ -0,0 +1,22 @@
+package Operators;
+
+public class PostIncrementAndDecrement {
+
+	public static void main(String[] args) {
+				//post increment
+		
+				int a=10;
+				int b=a++;
+				System.out.println(a);
+				System.out.println(b);
+				
+				// post Decrement
+				
+				int a1=10;
+				int b1=a1--;
+				System.out.println(a1);
+				System.out.println(b1);
+
+	}
+
+}
---
+++ Implement OOPS using JAVA with Data Structures and Beyond/Assignment-1/Operators/PreIncrementAndDecrement.java
@@ -0,0 +1,23 @@
+package Operators;
+
+public class PreIncrementAndDecrement 
+{
+
+	public static void main(String[] args)
+	{
+		//pre increment--First Value of a is increase and then assign to b
+		
+		int a=10;
+		int b=++a;
+		System.out.println(a);
+		System.out.println(b);
+		
+		// pre Decrement--First Value of a is Decrease and then assign to b
+		
+		int a1=10;
+		int b1=--a1;
+		System.out.println(a1);
+		System.out.println(b1);
+			
+	}
+}
