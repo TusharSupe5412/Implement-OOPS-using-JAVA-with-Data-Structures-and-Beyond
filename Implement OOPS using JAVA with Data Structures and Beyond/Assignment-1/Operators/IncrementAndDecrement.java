c24fa112e1c90ed8af675c83f5b2c6801b82673e
Tushar <tsupe222@gmail.com>
2021-12-14 11:33:53 +0530

    added Implement OOPS using JAVA with Data Structures and Beyond

---
+++ Implement OOPS using JAVA with Data Structures and Beyond/Assignment-1/Operators/IncrementAndDecrement.java
@@ -0,0 +1,21 @@
+package Operators;
+
+public class IncrementAndDecrement {
+
+	public static void main(String[] args) {
+		int a= 10;
+		System.out.println("Value entered is:"+ a);
+		{
+		a++;
+		System.out.println("value of a is Increment to:"+ a);
+		}
+		
+		{
+			a--;
+			System.out.println("Value of a is Decremented to:"+ a);
+		}
+		
+		
+	}
+
+}
